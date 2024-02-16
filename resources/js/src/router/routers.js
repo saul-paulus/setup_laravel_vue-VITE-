85a50590ae091f7000f256da2447bcd5a00838e5
saul-paulus <saulpaulus17@icloude.com>
2024-02-16 01:48:11 +0000

    build: install and config tailwind

--- resources/js/src/router/routers.js
+++ resources/js/src/router/routers.js
@@ -1,22 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Login from "../views/auth/Login.vue";
 
 const router = createRouter({
-    history: createWebHistory('/'),
-    routes : [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: About
-        }
-    ]
-})
+  history: createWebHistory("/"),
+  routes: [
+    {
+      path: "/auth/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+  ],
+});
 
-
-export default router
+export default router;
